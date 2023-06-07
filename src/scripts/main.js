e98aa75c2375bc9a98cb471bd5381a46a467d2af
Nickolas Abad <123907645+NickolasAbad@users.noreply.github.com>
2023-06-07 01:53:36 -0300

    criando o timer com o pretty-ms

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
-console.log("Ola mundo")
+import prettyMilliseconds from 'pretty-ms';
+
+AOS.init();
+
+// Definindo o timestamp do evento
+const dataDoEvento = new Date("Jul 29, 2023 00:00:00");
+const timestampDoEvento = dataDoEvento.getTime();
+
+// Função para cronometrar o tempo restante de acordo com a diferença de timestamps e alteração de DOM
+   const contador = setInterval(function() {
+        const agora = new Date();
+        const timestampAtual = agora.getTime();
+        const distanciaAteOEvento = timestampDoEvento - timestampAtual;
+    
+        if(distanciaAteOEvento > 0) {
+            var distanciaFormatada = prettyMilliseconds(distanciaAteOEvento, { secondsDecimalDigits: 0 })
+            document.querySelector('.hero__text').innerHTML = `O maior evento de tecnologia do Brasil começa em <span class='hero__text--counter'>${distanciaFormatada}<span/>`
+                } else {
+            clearInterval(contador);
+            const textoTimer = document.querySelector('.hero__text')
+            textoTimer.classList.add('hero__text--counter')
+            textoTimer.innerHTML = 'O evento já começou'
+        }
+    }, 1000)
